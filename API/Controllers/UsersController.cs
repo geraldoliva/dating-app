c61aca4eb1d177043763239c627d54e3efe41247
Gerald Oliva <geraldsamoliva@gmail.com>
2025-01-26 21:38:33 -0500

    refactor to add extension methods

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,7 +9,7 @@ namespace API.Controllers;
 
 public class UsersController(DataContext context) : BaseApiController
 {
-
+  [AllowAnonymous]
   [HttpGet]
   public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
   {
@@ -17,6 +18,7 @@ public class UsersController(DataContext context) : BaseApiController
     return users;
   }
 
+  [Authorize]
   [HttpGet("{id}")] // /api/users/1
   public async Task<ActionResult<AppUser>> GetUser(int id)
   {
