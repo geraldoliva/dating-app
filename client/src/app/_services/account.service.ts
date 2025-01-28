301f95249ae27eb3f0602f1792d5558730ad141c
Gerald Oliva <geraldsamoliva@gmail.com>
2025-01-27 21:44:12 -0500

    add routes

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -4,26 +4,38 @@ import { User } from '../_models/user';
 import { map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
-  private http = inject(HttpClient)
+  private http = inject(HttpClient);
   baseUrl = 'https://localhost:5001/api/';
-  currentUser = signal<User | null>(null)
+  currentUser = signal<User | null>(null);
 
   login(model: any) {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
-      map(user => {
+      map((user) => {
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user))
-          this.currentUser.set(user)
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
         }
       })
-    )
+    );
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map((user) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+        return user
+      })
+    );
   }
 
   logout() {
-    localStorage.removeItem('user')
-    this.currentUser.set(null)
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
   }
 }
