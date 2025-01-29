82fed073ad4db9813e905e4fef65061e1261182e
Gerald Oliva <geraldsamoliva@gmail.com>
2025-01-29 10:23:56 -0500

    add dto for members

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,5 @@
 using System;
+using API.Extensions;
 
 namespace API.Entities;
 
@@ -8,6 +9,21 @@ public class AppUser
 
   public required string UserName { get; set; }
 
-  public required byte[] PasswordHash { get; set; }
-  public required byte[] PasswordSalt { get; set; }
+  public byte[] PasswordHash { get; set; } = [];
+  public byte[] PasswordSalt { get; set; } = [];
+  public DateOnly DateOfBirth { get; set; }
+  public required string KnownAs { get; set; }
+  public DateTime Created { get; set; } = DateTime.UtcNow;
+  public DateTime LastActive { get; set; } = DateTime.UtcNow;
+  public required string Gender { get; set; }
+  public string? Interests { get; set; }
+  public string? LookingFor { get; set; }
+  public required string City { get; set; }
+  public required string Country { get; set; }
+  public List<Photo> Photo { get; set; } = [];
+
+  public int GetAge()
+  {
+    return DateOfBirth.CalculateAge();
+  }
 }
