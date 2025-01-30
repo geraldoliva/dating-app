a2a929852cc4129a20b1f93963cbd8a62bf8912a
Gerald Oliva <geraldsamoliva@gmail.com>
2025-01-30 17:07:30 -0500

    finish update functionality to member-edit

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { Member } from '../../_models/member';
 import { AccountService } from '../../_services/account.service';
 import { MembersService } from '../../_services/members.service';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
@@ -14,6 +14,7 @@ import { ToastrService } from 'ngx-toastr';
   styleUrl: './member-edit.component.css',
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm?: NgForm
   member?: Member;
   private accountService = inject(AccountService);
   private memberService = inject(MembersService);
@@ -34,5 +35,6 @@ export class MemberEditComponent implements OnInit {
   updateMember() {
     console.log(this.member)
     this.toastr.success('Profile updated successfully')
+    this.editForm?.reset(this.member)
   }
 }
