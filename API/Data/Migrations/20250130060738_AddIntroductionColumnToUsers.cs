e90252f2b0e9f96270415a3b2093682b6e3f6800
Gerald Oliva <geraldsamoliva@gmail.com>
2025-01-30 01:20:46 -0500

    add styles to member detailed page

---
+++ API/Data/Migrations/20250130060738_AddIntroductionColumnToUsers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIntroductionColumnToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Introduction",
+                table: "Users",
+                type: "TEXT",
+                nullable: true,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Introduction",
+                table: "Users");
+        }
+    }
+}
--- client/src/app/_models/member.ts
+++ client/src/app/_models/member.ts
@@ -9,6 +9,7 @@ export interface Member {
   created: Date;
   lastActive: Date;
   gender: string;
+  introduction: string;
   interests: string;
   lookingFor: string;
   city: string;
