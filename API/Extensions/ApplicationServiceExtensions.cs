6c7bfe59feebbf352c80ed12c555c69070d093d8
Gerald Oliva <geraldsamoliva@gmail.com>
2025-02-02 10:59:04 -0500

    add likes repository

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -20,8 +20,10 @@ public static class ApplicationServiceExtensions
       services.AddCors();
       services.AddScoped<ITokenService, TokenService>();
       services.AddScoped<IUserRepository, UserRepository>();
+      services.AddScoped<ILikesRepository, LikesRepository>();
       services.AddScoped<IPhotoService, PhotoService>();
       services.AddScoped<LogUserActivity>();
+
       services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
       services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
 
