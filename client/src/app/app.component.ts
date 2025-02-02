efc80487823e5d8aa5323f32be33301844e6044d
Gerald Oliva <geraldsamoliva@gmail.com>
2025-02-02 16:58:25 -0500

    add pagination for the likes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
-import { HomeComponent } from "./home/home.component";
+import { HomeComponent } from './home/home.component';
 import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
@@ -13,19 +13,17 @@ import { NgxSpinnerComponent } from 'ngx-spinner';
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  private accountService = inject(AccountService)
+  private accountService = inject(AccountService);
 
   ngOnInit(): void {
-    this.setCurrentUser()
+    this.setCurrentUser();
   }
 
   setCurrentUser() {
-    const userString = localStorage.getItem('user')
-    if (!userString) return
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
 
-    const user = JSON.parse(userString)
-    this.accountService.currentUser.set(user)
+    const user = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
-
-
 }
