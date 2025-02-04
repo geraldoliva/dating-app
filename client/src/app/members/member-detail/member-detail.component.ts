6e1ff0debbce1c48d80a5673f4653a57adedcea2
Gerald Oliva <geraldsamoliva@gmail.com>
2025-02-03 19:24:40 -0500

    delete messages on the client

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -24,38 +24,68 @@ import { MessageService } from '../../_services/message.service';
   styleUrl: './member-detail.component.css',
 })
 export class MemberDetailComponent implements OnInit {
-  @ViewChild('memberTabs') memberTabs?: TabsetComponent;
+  @ViewChild('memberTabs', {static: true}) memberTabs?: TabsetComponent;
   private memberService = inject(MembersService);
   private route = inject(ActivatedRoute);
-  private messageService = inject(MessageService)
-  member?: Member;
+  private messageService = inject(MessageService);
+  member: Member = {} as Member
   images: GalleryItem[] = [];
   activeTab?: TabDirective;
   messages: Message[] = [];
 
   ngOnInit(): void {
-    this.loadMember();
+    this.route.data.subscribe({
+      next: (data) => {
+        this.member = data['member'];
+        this.member && this.member.photos.map((p) => {
+          this.images.push(new ImageItem({ src: p.url, thumb: p.url }));
+        });
+      },
+    });
+
+    this.route.queryParams.subscribe({
+      next: (params) => {
+        params['tab'] && this.selectTab(params['tab']);
+      },
+    });
+  }
+
+  onUpdateMessages(event: Message) {
+    this.messages.push(event)
+  }
+
+  selectTab(heading: string) {
+    if (this.memberTabs) {
+      const messageTab = this.memberTabs.tabs.find(
+        (x) => x.heading === heading
+      );
+      if (messageTab) messageTab.active = true;
+    }
   }
 
   onTabActivated(data: TabDirective) {
     this.activeTab = data;
-    if (this.activeTab.heading === 'Messages' && this.messages.length === 0 && this.member) {
+    if (
+      this.activeTab.heading === 'Messages' &&
+      this.messages.length === 0 &&
+      this.member
+    ) {
       this.messageService.getMessageThread(this.member.username).subscribe({
         next: (messages) => (this.messages = messages),
       });
     }
   }
 
-  loadMember() {
-    const username = this.route.snapshot.paramMap.get('username');
-    if (!username) return;
-    this.memberService.getMember(username).subscribe({
-      next: (member) => {
-        this.member = member;
-        member.photos.map((p) => {
-          this.images.push(new ImageItem({ src: p.url, thumb: p.url }));
-        });
-      },
-    });
-  }
+  // loadMember() {
+  //   const username = this.route.snapshot.paramMap.get('username');
+  //   if (!username) return;
+  //   this.memberService.getMember(username).subscribe({
+  //     next: (member) => {
+  //       this.member = member;
+  //       member.photos.map((p) => {
+  //         this.images.push(new ImageItem({ src: p.url, thumb: p.url }));
+  //       });
+  //     },
+  //   });
+  // }
 }
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -10,7 +10,13 @@ import { PaginationModule } from 'ngx-bootstrap/pagination';
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [ButtonsModule, FormsModule, TimeagoModule, RouterLink, PaginationModule],
+  imports: [
+    ButtonsModule,
+    FormsModule,
+    TimeagoModule,
+    RouterLink,
+    PaginationModule,
+  ],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css',
 })
@@ -19,6 +25,7 @@ export class MessagesComponent implements OnInit {
   container = 'Inbox';
   pageNumber = 1;
   pageSize = 5;
+  isOutbox = this.container === 'Outbox';
 
   ngOnInit(): void {
     this.loadMessages();
@@ -32,9 +39,24 @@ export class MessagesComponent implements OnInit {
     );
   }
 
+  deleteMessage(id: number) {
+    this.messageService.deleteMessage(id).subscribe({
+      next: (_) => {
+        this.messageService.paginatedResult.update((prev) => {
+          if (prev && prev.items) {
+            prev.items.splice(prev.items.findIndex((m) => m.id === id), 1);
+            return prev;
+          }
+          return prev;
+        });
+      },
+    });
+  }
+
   getRoute(message: Message) {
-    if (this.container === 'Outbox') return `/members/${message.recipientUsername}`
-    else return `/members/${message.senderUsername}`
+    if (this.container === 'Outbox')
+      return `/members/${message.recipientUsername}`;
+    else return `/members/${message.senderUsername}`;
   }
 
   pageChanged(event: any) {
