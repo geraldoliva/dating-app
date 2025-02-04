0aeb6eaca2dfc8a87998d250af812c185e1bfbc0
Gerald Oliva <geraldsamoliva@gmail.com>
2025-02-03 23:24:00 -0500

    add policy based authorization

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -31,7 +31,7 @@ public class AccountController(UserManager<AppUser> userManager, ITokenService t
     return new UserDto
     {
       Username = user.UserName,
-      Token = tokenService.CreateToken(user),
+      Token = await tokenService.CreateToken(user),
       KnownAs = user.KnownAs,
       Gender = user.Gender
     };
@@ -54,7 +54,7 @@ public class AccountController(UserManager<AppUser> userManager, ITokenService t
     {
       Username = user.UserName,
       KnownAs = user.KnownAs,
-      Token = tokenService.CreateToken(user),
+      Token = await tokenService.CreateToken(user),
       Gender = user.Gender,
       PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
     };
