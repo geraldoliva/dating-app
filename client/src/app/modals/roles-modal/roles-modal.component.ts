a933996c72b6c58148a356d217914ee0ca267044
Gerald Oliva <geraldsamoliva@gmail.com>
2025-02-04 11:06:36 -0500

    edit roles

--- client/src/app/modals/roles-modal/roles-modal.component.ts
+++ client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -6,10 +6,26 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
   standalone: true,
   imports: [],
   templateUrl: './roles-modal.component.html',
-  styleUrl: './roles-modal.component.css'
+  styleUrl: './roles-modal.component.css',
 })
 export class RolesModalComponent {
-  bsModalRef = inject(BsModalRef)
-  title = ''
-  list: string[] = []
+  bsModalRef = inject(BsModalRef);
+  username = '';
+  title = '';
+  availableRoles: string[] = [];
+  selectedRoles: string[] = [];
+  rolesUpdated = false;
+
+  updateChecked(checkedValue: string) {
+    if (this.selectedRoles.includes(checkedValue)) {
+      this.selectedRoles = this.selectedRoles.filter((r) => r !== checkedValue);
+    } else {
+      this.selectedRoles.push(checkedValue);
+    }
+  }
+
+  onSelectRoles() {
+    this.rolesUpdated = true
+    this.bsModalRef.hide()
+  }
 }
