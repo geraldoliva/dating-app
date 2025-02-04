e8d1504916e09512c4330252b3344ab7318d5eb1
Gerald Oliva <geraldsamoliva@gmail.com>
2025-02-04 01:49:39 -0500

    add edit roles component

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -19,7 +19,7 @@ public class AdminController(UserManager<AppUser> userManager) : BaseApiControll
       .Select(x => new
       {
         x.Id,
-        UserName = x.UserName,
+        Username = x.UserName,
         Roles = x.UserRoles.Select(r => r.Role.Name).ToList()
       }).ToListAsync();
 
